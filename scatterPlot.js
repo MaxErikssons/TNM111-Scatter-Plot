a7c285c68609ca37018e8dc9a04a22240f04f965
Max Eriksson <maxer142@student.liu.se>
2023-01-26 11:14:48 +0100

    Scaling
    The scaling kind of works

--- scatterPlot.js
+++ scatterPlot.js
@@ -14,7 +14,7 @@ function createPlot(data) {
   } else {
     var minMaxValues = calculateMinMaxValues(data);
     drawGrid(ctx, canvas, minMaxValues);
-    drawAxis(ctx, canvas);
+    drawAxis(ctx, canvas, minMaxValues);
     addTicks(ctx, canvas, minMaxValues);
     plotData(ctx, canvas, data, minMaxValues);
   }
@@ -62,55 +62,68 @@ function calculateMinMaxValues(data) {
 }
 
 //Draw the x- and y-axis.
-function drawAxis(ctx, canvas) {
+function drawAxis(ctx, canvas, minMaxValues) {
+  var xyScale = scale(canvas, minMaxValues);
+  var xyRange = calculateRange(minMaxValues);
+
   // Draw the x-axis
   ctx.beginPath();
 
-  ctx.moveTo(0, canvas.height / 2);
-  ctx.lineTo(canvas.width, canvas.height / 2);
+  ctx.moveTo(0, xyScale.y * minMaxValues.yMax);
+  ctx.lineTo(xyScale.x * xyRange.x, xyScale.y * minMaxValues.yMax);
 
   // Draw the y-axis
-  ctx.moveTo(canvas.width / 2, 0);
-  ctx.lineTo(canvas.width / 2, canvas.height);
-
+  ctx.moveTo(xyScale.x * Math.abs(minMaxValues.xMin), 0);
+  ctx.lineTo(xyScale.x * Math.abs(minMaxValues.xMin), xyScale.y * xyRange.y);
+  console.log(xyScale.y * xyRange.y);
   ctx.strokeStyle = 'black';
   ctx.stroke();
 }
 
 //Draw the grid.
 function drawGrid(ctx, canvas, minMaxValues) {
-  //calculate the range of x and y
-  var xRange = minMaxValues.xMax - minMaxValues.xMin;
-  var yRange = minMaxValues.yMax - minMaxValues.yMin;
+  var xyScale = scale(canvas, minMaxValues);
+  var xyRange = calculateRange(minMaxValues);
 
   //determine the number of grid lines and the spacing
-  var xLines = Math.round(xRange / 10);
-  var yLines = Math.round(yRange / 10);
-  var xSpace = Math.round(canvas.width / xLines);
-  var ySpace = Math.round(canvas.height / yLines);
+  var xLines = Math.round(xyRange.x / 10);
+  var yLines = Math.round(xyRange.y / 10);
+  var xSpace = (xyScale.x * minMaxValues.xMax) / xLines;
+  var ySpace = (xyScale.y * Math.abs(minMaxValues.yMin)) / yLines;
 
   ctx.beginPath();
   //draw the vertical grid lines
-  for (var x = canvas.width / 2; x <= canvas.width; x += xSpace) {
+  for (
+    var x = xyScale.x * Math.abs(minMaxValues.xMin) + xSpace;
+    x <= xyScale.x * xyRange.x;
+    x += xSpace
+  ) {
     ctx.moveTo(x, 0);
-    ctx.lineTo(x, canvas.height);
+    ctx.lineTo(x, xyScale.y * xyRange.y);
   }
 
-  for (var x = canvas.width / 2; x >= 0; x -= xSpace) {
+  for (
+    var x = xyScale.x * Math.abs(minMaxValues.xMin) - xSpace;
+    x >= 0;
+    x -= xSpace
+  ) {
     ctx.moveTo(x, 0);
-    ctx.lineTo(x, canvas.height);
+    ctx.lineTo(x, xyScale.y * xyRange.y);
   }
 
   //draw the horizontal grid lines
-  for (var y = canvas.height / 2; y <= canvas.height; y += ySpace) {
+  for (
+    var y = xyScale.y * minMaxValues.yMax + ySpace;
+    y < xyScale.y * xyRange.y;
+    y += ySpace
+  ) {
     ctx.moveTo(0, y);
-    ctx.lineTo(canvas.width, y);
+    ctx.lineTo(xyScale.x * xyRange.x, y);
   }
 
-  //draw the horizontal grid lines
-  for (var y = canvas.height / 2; y >= 0; y -= ySpace) {
+  for (var y = xyScale.y * minMaxValues.yMax - ySpace; y > 0; y -= ySpace) {
     ctx.moveTo(0, y);
-    ctx.lineTo(canvas.width, y);
+    ctx.lineTo(xyScale.x * xyRange.x, y);
   }
 
   //set the grid style and draw the lines
@@ -121,57 +134,76 @@ function drawGrid(ctx, canvas, minMaxValues) {
 //Draw ticks
 function addTicks(ctx, canvas, minMaxValues) {
   //Calculate the number of tick marks and the spacing for the x- and y-axis
-  var xRange = minMaxValues.xMax - minMaxValues.xMin;
-  var xTickMarks = Math.round(xRange / 10);
-  var xTickSpace = Math.round(canvas.width / xTickMarks);
+  var xyScale = scale(canvas, minMaxValues);
+  var xyRange = calculateRange(minMaxValues);
 
-  var yRange = minMaxValues.yMax - minMaxValues.yMin;
-  var yTickMarks = Math.round(yRange / 10);
-  var yTickSpace = Math.round(canvas.height / yTickMarks);
+  var xTickMarks = Math.round(xyRange.x / 10);
+  var yTickMarks = Math.round(xyRange.y / 10);
 
+  var xTickSpace = (xyScale.x * minMaxValues.xMax) / xTickMarks;
+  var yTickSpace = (xyScale.y * Math.abs(minMaxValues.yMin)) / yTickMarks;
+
+  var xTickValue = Math.round(minMaxValues.xMax / xTickMarks);
+  var yTickValue = Math.round(minMaxValues.yMax / yTickMarks);
   var counter = 1;
 
   ctx.beginPath();
   //Draw the tick marks on the x-axis
   for (
-    var x = canvas.width / 2 + xTickSpace;
-    x <= canvas.width;
+    var x = xyScale.x * Math.abs(minMaxValues.xMin) + xTickSpace;
+    x <= xyScale.x * xyRange.x;
     x += xTickSpace
   ) {
     ctx.moveTo(x, canvas.height / 2 - 5);
     ctx.lineTo(x, canvas.height / 2 + 5);
-    ctx.fillText(xTickMarks * counter, x, canvas.height / 2 + 15);
+    ctx.fillText(xTickValue * counter, x, canvas.height / 2 + 15);
     counter++;
   }
 
   counter = -1;
   //Draw the tick marks on the x-axis
-  for (var x = canvas.width / 2 - xTickSpace; x >= 0; x -= xTickSpace) {
-    ctx.moveTo(x, canvas.height / 2 - 5);
-    ctx.lineTo(x, canvas.height / 2 + 5);
-    ctx.fillText(xTickMarks * counter, x, canvas.height / 2 + 15);
+  for (
+    var x = xyScale.x * Math.abs(minMaxValues.xMin) - xTickSpace;
+    x >= 0;
+    x -= xTickSpace
+  ) {
+    ctx.moveTo(x, xyScale.y * minMaxValues.yMax - 5);
+    ctx.lineTo(x, xyScale.y * minMaxValues.yMax + 5);
+    ctx.fillText(xTickValue * counter, x, xyScale.y * minMaxValues.yMax + 15);
     counter--;
   }
 
   counter = -1;
   //Draw the tick marks on the y-axis
   for (
-    var y = canvas.height / 2 + yTickSpace;
-    y <= canvas.height;
+    var y = xyScale.y * minMaxValues.yMax + yTickSpace;
+    y < xyScale.y * xyRange.y;
     y += yTickSpace
   ) {
-    ctx.moveTo(canvas.width / 2 - 5, y);
-    ctx.lineTo(canvas.width / 2 + 5, y);
-    ctx.fillText(yTickMarks * counter, canvas.width / 2 - 20, y);
+    ctx.moveTo(xyScale.x * Math.abs(minMaxValues.xMin) - 5, y);
+    ctx.lineTo(xyScale.x * Math.abs(minMaxValues.xMin) + 5, y);
+    ctx.fillText(
+      yTickValue * counter,
+      xyScale.x * Math.abs(minMaxValues.xMin) - 20,
+      y
+    );
     counter--;
   }
 
   counter = 1;
   //Draw the tick marks on the y-axis
-  for (var y = canvas.height / 2 - yTickSpace; y >= 0; y -= yTickSpace) {
-    ctx.moveTo(canvas.width / 2 - 5, y);
-    ctx.lineTo(canvas.width / 2 + 5, y);
-    ctx.fillText(yTickMarks * counter, canvas.width / 2 - 20, y);
+  for (
+    var y = xyScale.y * minMaxValues.yMax - yTickSpace;
+    y >= 0;
+    y -= yTickSpace
+  ) {
+    ctx.moveTo(xyScale.x * Math.abs(minMaxValues.xMin) - 5, y);
+    ctx.lineTo(xyScale.x * Math.abs(minMaxValues.xMin) + 5, y);
+    ctx.fillText(
+      yTickValue * counter,
+      xyScale.x * Math.abs(minMaxValues.xMin) - 20,
+      y
+    );
     counter++;
   }
 
@@ -181,21 +213,15 @@ function addTicks(ctx, canvas, minMaxValues) {
 
 //Plot the data on the grid.
 function plotData(ctx, canvas, data, minMaxValues) {
-  var xMin = minMaxValues.xMin;
-  var yMin = minMaxValues.yMin;
-  var xMax = minMaxValues.xMax;
-  var yMax = minMaxValues.yMax;
-
-  //calculate the range of x and y
-  var xRange = xMax - xMin;
-  var yRange = yMax - yMin;
+  var xyScale = scale(canvas, minMaxValues);
 
   // Plot the data
   for (var i = 0; i < data.length; i++) {
     // Get the x and y values.
     //DETTA ÄR HÅRDKODAT OCH ÄR INTE ALLTID SKALENLIGT! Vet inte hur det ska lösas.
-    var x = ((data[i].x + 60) / 120) * canvas.width;
-    var y = canvas.height - ((data[i].y + 60) / 120) * canvas.height;
+    console.log(canvas.height - (data[i].y - minMaxValues.yMin) * xyScale.y);
+    var x = (data[i].x - minMaxValues.xMin) * xyScale.x;
+    var y = canvas.height - (data[i].y - minMaxValues.yMin) * xyScale.y;
     var color = data[i].color;
 
     // Draw a circle at the x and y position
@@ -205,4 +231,28 @@ function plotData(ctx, canvas, data, minMaxValues) {
     ctx.fill();
     ctx.fillText('(' + data[i].x + ',' + data[i].y + ')', x + 8, y + 8);
   }
+}
+
+function scale(canvas, minMaxValues) {
+  //calculate the range of x and y
+  var xyRange = calculateRange(minMaxValues);
+
+  //calculate the scale of x and y
+  var x = canvas.width / xyRange.x;
+  var y = canvas.height / xyRange.y;
+
+  return { x, y };
+}
+
+function calculateRange(minMaxValues) {
+  var xMin = minMaxValues.xMin;
+  var yMin = minMaxValues.yMin;
+  var xMax = minMaxValues.xMax;
+  var yMax = minMaxValues.yMax;
+
+  //calculate the range of x and y
+  var x = xMax - xMin;
+  var y = yMax - yMin;
+
+  return { x, y };
 }
